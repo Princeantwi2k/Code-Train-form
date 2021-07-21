5e94c696d85440a707b896c62cd77a48aba6e74e
prince antwi <antwi2k@gmail.com>
2021-07-21 13:14:16 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col } from 'react-bootstrap';
+import React, { Component } from 'react';
+import UserForm from './Component/UserForm';
+import AddUser from './Component/AddUser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   constructor(props){
+     super(props)
+     this.state = {
+       users: [
+       
+       ]
+     }
+   }
+  
+   adduser = (user) =>{
+     user.id= Math.random().toString
+     this.setState({
+       users: [...this.state.users, user]
+     })
+   }
+   handledelete =(id) =>{
+    let undeleted = this.state.users.filter((user) => user.id !== id)
+   this.setState({ 
+     users: undeleted
+   })
+   }
+   handleEidth =(id, updateUser) =>{
+    this.setState({
+      users : this.state.users.map(user=> user.id === id ? updateUser: user)
+    })
+    }
+  render() {
+    return (
+      <>
+      <Container fluid style={{ marginTOP: `30px` }}>
+        <Row>
+          <Col xs={4}  >
+            <AddUser newuser={this.adduser}/>
+          </Col>
+          <Col xs={4}  >
+            <UserForm  usersDater={this.state.users} deleteuser={this.handledelete} eidthuser={this.handleEidth}/>
+          </Col>
+        </Row>
+      </Container>
+      </>
+    );
+  }
 }
 
 export default App;
